3c6503bd02f6181d8ea25957bdb356bdb8130f0e
Gowthamiselvam <107820024+Gowthamiselvam@users.noreply.github.com>
2022-07-18 23:56:32 +1000

    Add files via upload

---
+++ 2nd Assesessment/Ticket booking app/index.js
@@ -0,0 +1,10 @@
+function Submit() {
+    alert("Your Booking is Confirmed");
+    }
+
+function validFormFieldInput()
+{
+    const phoneInputField = document.getElementbyId("#mobile");
+    const EmailInputField = document.getElementById("#Email");
+    console.log(validFormFieldInput());
+}
